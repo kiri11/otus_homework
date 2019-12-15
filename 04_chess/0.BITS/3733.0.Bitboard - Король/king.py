106d133dee4c31c3ef7b408b5e98500d511e694c
kiri11 <kirill.ign@gmail.com>
2019-12-15 13:43:27 +0000

    Solve king problem

---
+++ 04_chess/0.BITS/3733.0.Bitboard - Король/king.py
@@ -0,0 +1,16 @@
+class King:
+    nA = 0xFeFeFeFeFeFeFeFe
+    nH = 0x7f7f7f7f7f7f7f7f
+    fullBoard = 0xffffffffffffffff
+
+    def __init__(self, position):
+        self.position = 1 << position
+
+    def valid_moves(self):
+        return ((self.position << 8 | self.position >> 8)
+                | self.nH & (self.position >> 9 | self.position >> 1 | self.position << 7)
+                | self.nA & (self.position >> 7 | self.position << 1 | self.position << 9)) \
+                & self.fullBoard
+
+    def __str__(self):
+        return "\n".join(str(getattr(self, figure)) for figure in "PNBRQKpnbrqk")
