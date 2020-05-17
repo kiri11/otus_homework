b0e432996523a75b0dab8fb4245b7e72c0447bf4
kiri11 <kirill.ign@gmail.com>
2020-05-17 11:24:50 +0100

    Add SingleArray

---
+++ 04_array/DynamicArrays/src/model/SingleArray.java
@@ -0,0 +1,46 @@
+package model;
+
+public class SingleArray<T> implements IArray<T> {
+
+    private Object[] array;
+
+    public SingleArray () {
+        array = new Object[0];
+    }
+
+    @Override
+    public int size() {
+        return array.length;
+    }
+
+    @Override
+    public void add(T item) {
+        add(item, size());
+    }
+
+    // @Override
+    public void add(T item, int index) {
+        Object[] newArray = new Object[size() + 1];
+        System.arraycopy(array, 0, newArray, 0, index);
+        newArray[index] = item;
+        System.arraycopy(array, index, newArray, index+1, size()-index);
+        array = newArray;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        return (T)array[index];
+    }
+
+    // @Override
+    public T remove(int index){
+        Object[] newArray = new Object[size() - 1];
+        System.arraycopy(array, 0, newArray, 0, index);
+        T res = get(index);
+        System.arraycopy(array, index, newArray, index+1, size()-index-1);
+        array = newArray;
+        return res;
+    }
+
+}
