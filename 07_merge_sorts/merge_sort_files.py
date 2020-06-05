fb63efa51be05b359d35f52d4eca43088ba24c92
kiri11 <kirill.ign@gmail.com>
2020-06-05 10:48:30 +0100

    Smaller chunks and remove debugging

--- 07_merge_sorts/merge_sort_files.py
+++ 07_merge_sorts/merge_sort_files.py
@@ -3,7 +3,7 @@ from timeit import timeit
 from tempfile import TemporaryFile
 from bin_num_file import read_bin_file_in_full, pack_format, SIZEOF_NUM
 
-CHUNK_SIZE = 2**28
+CHUNK_SIZE = 1024
 
 
 def write_to_temp(arr):
@@ -11,7 +11,6 @@ def write_to_temp(arr):
     res_file = TemporaryFile(mode='r+b')
     res_file.write(packed)
     res_file.seek(0)
-    print("Wrote chunk of size %i bytes" % len(packed))
     return res_file
 
 
@@ -30,7 +29,6 @@ def merge_file_sort(file, left=0, right=None):
     a = merge_file_sort(file, left, mid)
     b = merge_file_sort(file, mid, right)
     merged = merge(a, b)
-    print("merged")
     a.close()
     b.close()
     return merged
@@ -38,7 +36,6 @@ def merge_file_sort(file, left=0, right=None):
 
 def merge(file_a, file_b):
     """Merge two sorted files into one, maintaining sorting."""
-    print("merge started")
     res = []
     a = read_bin_file_in_full(file_a)
     b = read_bin_file_in_full(file_b)
