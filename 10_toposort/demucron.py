78218f04047c5bbb985f4a7c4f967e9d2fc99aea
kiri11 <kirill.ign@gmail.com>
2020-07-14 23:36:45 +0100

    Demurcon homework version

--- 10_toposort/demucron.py
+++ 10_toposort/demucron.py
@@ -1,26 +1,12 @@
 from typing import List
 
 
-def make_graph(edges, n):
-    g = [[] for _ in range(n)]
-    for a, b in edges:
-        g[a].append(b)
-    return g
-
-
-def graph_to_adj_matrix(graph: list):
+def graph_to_adj_matrix(graph: List[List[int]]):
     n = len(graph)
     adj_matrix = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in graph[i]:
             adj_matrix[i][j] = 1
-    return adj_matrix
-
-
-def edges_to_adj_matrix(n: int, edges: list):
-    adj_matrix = [[0] * n for _ in range(n)]
-    for a, b in edges:
-        adj_matrix[b][a] = 1
     return adj_matrix
 
 
@@ -44,13 +30,6 @@ def demurcon(adj_matrix: List[List[int]]):
     return result
 
 
-class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = make_graph(prerequisites, numCourses)
-        adj_matrix = graph_to_adj_matrix(graph)
-        return demurcon(adj_matrix)
-
-
-print(Solution().findOrder(2, [[1,0],] ))
-print(Solution().findOrder(4, [[1,0],[2,0],[3,1],[3,2]]))
-print(Solution().findOrder(2, [[0,1],[1,0]]))
+def toposort(graph: List[List[int]]) -> List[int]:
+    adj_matrix = graph_to_adj_matrix(graph)
+    return demurcon(adj_matrix)
